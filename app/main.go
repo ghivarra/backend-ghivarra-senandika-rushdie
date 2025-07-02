af30d10419210012ff30a5ea2da1bfd200d50336
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-07-02 20:37:59 +0700

    add root

--- app/main.go
+++ app/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"log"
+	"path"
 
 	"github.com/ghivarra/app/module/router"
 	"github.com/ghivarra/app/server"
 	"github.com/joho/godotenv"
 )
 
+var ROOT string
+
 func main() {
+
+	// put root
+	ROOT = path.Clean("/")
 
 	// load environment
 	errEnv := godotenv.Load("./.env")
